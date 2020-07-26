a3b3b6c249789833634f0bd591f408e7b93fa7e6
Andrei Dumitrescu <andumywow@gmail.com>
2020-07-26 04:11:13 +0300

    added extra explanation to curs_2

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 // set this to true when you think you are ready(only for CHAL_2).
-// the code will be auto-checked, but be aware, if you have bugs you will receive penality points! 
-
 $checker = false;
+// the code will be auto-checked, but be aware, if you have bugs you will receive penality points! 
+// when you set $checker = true; you must think as if the application is public and available to users
+// so any bug can bring multiple issues.
+
 require_once './bin/bootloader.php';
 
 // write your code between these lines
