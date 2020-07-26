30a1766c04666327998ceb91c8c2201c35629dd9
Andrei Dumitrescu <andumywow@gmail.com>
2020-07-26 03:36:15 +0300

    finished curs_2

--- bin/bootloader.php
+++ bin/bootloader.php
@@ -5,13 +5,33 @@
  * @usage: Include this file at the root of the project
  * @author Andrei Dumitrescu
  */
-    require_once 'credentials.php';
-    $db = new mysqli(HOST,USER,SECRET,DB,PORT);
+    if($checker){
+        require_once 'credentials.php';
+        $db = new mysqli(HOST,USER,SECRET,DB,PORT);
+    
+        if(function_exists('getPrice')){
+            $data = $db->query('SELECT * FROM Market')->fetch_all();
+            $err = 0;
+            foreach($data as $try){
+                $basket = getPrice($try[1],$try[2],$try[3],$try[4]);
+                if($basket != $try[5] || is_null($basket) || !isset($basket)){
+                    $err++;
+                }
+            }
 
+            $branch_name = explode("/", file('.git/HEAD', FILE_USE_INCLUDE_PATH)[0], 3)[2];
+            $status = $err == 0 ? 'Success' : 'Fail' ; 
+            $query = "INSERT INTO Results (person,status,curs,failed_tests) Values ('".$branch_name."','".$status."',2,".$err.")";
+            $result = $db->query($query);
 
-    $result = $db->query('SELECT * FROM Market')->fetch_assoc();
-    var_dump($result);
+            echo count($data)-$err.' out of '.count($data).' tests have passed';
 
-    $branch_name = explode("/", file('.git/HEAD', FILE_USE_INCLUDE_PATH)[0], 3)[2];
-    echo ($branch_name);
+            if($err == 0){
+                echo "</br>Congrats!</br>Don't forget to push the code to git.";   
+            }
+    
+        } else {
+            echo 'Function getPrice is not implemented';
+        }
+    }
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
+// set this to true when you think you are ready(only for CHAL_2).
+// the code will be auto-checked, but be aware, if you have bugs you will receive penality points! 
+
+$checker = false;
 require_once './bin/bootloader.php';
 
+// write your code between these lines
+//---------------------------------
 
 
+
+//---------------------------------
 ?>
