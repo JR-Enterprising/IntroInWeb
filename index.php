c3342118fd95c7afe9d6cd811271ebb1105928d0
ciobanugeorge <ciobanugeorge98@gmail.com>
2020-07-26 23:03:00 +0300

    CHAL_2 Solved

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // set this to true when you think you are ready(only for CHAL_2).
-$checker = false;
+$checker = true;
 // the code will be auto-checked, but be aware, if you have bugs you will receive penality points! 
 // when you set $checker = true; you must think as if the application is public and available to users
 // so any bug can bring multiple issues.
@@ -12,13 +12,29 @@ require_once './bin/bootloader.php';
 //---------------------------------
 
 function getPrice($jeans_amount, $shirts_amount, $shoes_amount, $skirts_amount){
-    $total_price = $jeans_amount*30 + $shirts_amount*20 + $shoes_amount*25 + $skirts_amount*50;
+    $total_price = 0;
+    if($jeans_amount >= 3){
+        $total_price += ((int)($jeans_amount / 3)) * 130;
+        $total_price += ($jeans_amount % 3) * 50;
+    }
+    else{
+        $total_price += $jeans_amount * 50;
+    }
+    if($shirts_amount >= 2){
+        $total_price += ((int)($shirts_amount / 2)) * 45;
+        $total_price += ($shirts_amount % 2) * 30;
+    }
+    else{
+        $total_price += $shirts_amount * 30;
+    }
+    $total_price += $shoes_amount * 20 + $skirts_amount * 15;
     return $total_price;
 }
 
-$basket_price = getPrice(5,2,1,5);
+$basket_price = getPrice(5,7,3,2);
 echo("Your basket total is ");
 echo($basket_price);
+
 
 //---------------------------------
 ?>
