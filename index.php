f48e6be792d0ed4c84ea1ee30296df82a3697596
Andrei Dumitrescu <andumywow@gmail.com>
2020-08-02 14:16:06 +0300

    solution for task 2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // set this to true when you think you are ready(only for CHAL_2).
-$checker = false;
+$checker = true;
 // the code will be auto-checked, but be aware, if you have bugs you will receive penality points! 
 // when you set $checker = true; you must think as if the application is public and available to users
 // so any bug can bring multiple issues.
@@ -11,9 +11,18 @@ require_once './bin/bootloader.php';
 // write your code between these lines
 //---------------------------------
 
+// 7 jeans -> 2 oferte de 130 + 1 pereche 50
+
+
+
 function getPrice($jeans_amount, $shirts_amount, $shoes_amount, $skirts_amount){
 
-    $total_price = $jeans_amount*30 + $shirts_amount*20 + $shoes_amount*25 + $skirts_amount*50;
+    $total_price = (int)($jeans_amount/3) * 130 + 
+                        ($jeans_amount%3) * 50 + 
+                    (int)($shirts_amount/2) * 45 + 
+                        ($shirts_amount%2) * 30 +
+                    $shoes_amount * 20 +
+                    $skirts_amount * 15;
 
     return $total_price;
 
