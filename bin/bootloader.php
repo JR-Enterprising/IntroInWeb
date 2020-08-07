aad444bb14c8ddf8eed503e2cac41ef0a3b4d171
pincuiulia <68693803+pincuiulia@users.noreply.github.com>
2020-08-07 06:46:25 -0700

    Add files via upload

--- bin/bootloader.php
+++ bin/bootloader.php
@@ -10,18 +10,19 @@
         $db = new mysqli(HOST,USER,SECRET,DB,PORT);
     
         if(function_exists('getPrice')){
-            $data = $db->query('SELECT * FROM Market')->fetch_all();
+            $data = $db->query('SELECT input,result FROM Market_Array')->fetch_all();
             $err = 0;
             foreach($data as $try){
-                $basket = getPrice($try[1],$try[2],$try[3],$try[4]);
-                if($basket != $try[5] || is_null($basket) || !isset($basket)){
+                $try[0] = $try[0] == '' ? [] : str_split($try[0]);
+                $basket = getPrice($try[0]);
+                if($basket != $try[1] || is_null($basket) || !isset($basket)){
                     $err++;
                 }
             }
 
             $branch_name = explode("/", file('.git/HEAD', FILE_USE_INCLUDE_PATH)[0], 3)[2];
             $status = $err == 0 ? 'Success' : 'Fail' ; 
-            $query = "INSERT INTO Results (person,status,curs,failed_tests) Values ('".$branch_name."','".$status."',2,".$err.")";
+            $query = "INSERT INTO Results (person,status,curs,failed_tests) Values ('".$branch_name."','".$status."',3,".$err.")";
             $result = $db->query($query);
 
             echo count($data)-$err.' out of '.count($data).' tests have passed';
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-// set this to true when you think you are ready(only for CHAL_2).
-$checker = true;
-// the code will be auto-checked, but be aware, if you have bugs you will receive penality po(int)s! 
+// set this to true when you think you are ready(only for CHAL_4).
+$checker = false;
+// the code will be auto-checked, but be aware, if you have bugs you will receive penality points! 
 // when you set $checker = true; you must think as if the application is public and available to users
 // so any bug can bring multiple issues.
 
@@ -10,35 +10,9 @@ require_once './bin/bootloader.php';
 
 // write your code between these lines
 //---------------------------------
-/*
-
-function $getPrice($jeans_amount, $shirts_amount, $shoes_amount, $skirts_amount){
-
-    $total_price = $jeans_amount*30 + $shirts_amount*20 + $shoes_amount*25 + $skirts_amount*50;
-
-    return $total_price;
-
-}
 
 
-    $basket_price = $getPrice(5,2,1,5);
-    echo($basket_price);
-*/
 
-function getPrice($jeans_amount, $shirts_amount, $shoes_amount, $skirts_amount){
-    if( $jeans_amount>3 && $shirts_amount>2){
-        $price= (int)($jeans_amount/3)*130+($jeans_amount-(int)($jeans_amount/3)*3)*50+(int)($shirts_amount/2)*45+($shirts_amount-(int)($shirts_amount/2)*2)*30+$shoes_amount*20+$skirts_amount*15;
-    }
-        else if( $jeans_amount>3 && $shirts_amount<2){
-             $price= (int)($jeans_amount/3)*130+($jeans_amount-(int)($jeans_amount/3)*3)*50+$shirts_amount*30+$shoes_amount*20+$skirts_amount*15;
-        }
-        else if($jeans_amount<3 && $shirts_amount>2){
-            $price= $jeans_amount*50+(int)($shirts_amount/2)*45+($shirts_amount-(int)($shirts_amount/2)*2)*30+$shoes_amount*20+$skirts_amount*15;
-        }
-        return $price;
-}
 
-echo("Your basket total is ");
-echo(getPrice(4,1,3,2));
 //---------------------------------
 ?>
