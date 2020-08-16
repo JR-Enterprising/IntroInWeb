582fa66ea753210d7632518f5ad24321e134a004
pincuiulia <68693803+pincuiulia@users.noreply.github.com>
2020-08-15 23:25:42 -0700

    Update credentials.php

--- bin/credentials.php
+++ bin/credentials.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 
 define('HOST','138.68.102.177');
 define('USER','mexSZ8zr7vmdAVVg');
 define('SECRET','6yfyVGJuKHBvUb8g');
 define('DB','webinar');
-=======
-<?php
-
-define('HOST','138.68.102.177');
-define('USER','mexSZ8zr7vmdAVVg');
-define('SECRET','6yfyVGJuKHBvUb8g');
-define('DB','webinar');
->>>>>>> origin/curs_4
 define('PORT','3306');
